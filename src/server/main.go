3a563c0e04d276d75e12e36376a38263637a7ddd
Oni-Men <sensyaheis@gmail.com>
2021-09-01 20:53:13 +0900

    rm q.Print()

--- src/server/main.go
+++ src/server/main.go
@@ -73,7 +73,6 @@ func handleClient(writer http.ResponseWriter, req *http.Request) {
 		switch ctx.Type {
 		case network.FindOpponent:
 			handleFindOpponent(ctx, p)
-			q.Print()
 		case network.ClickBoard:
 			handleClickBoard(ctx, p)
 		case network.Spectate:
